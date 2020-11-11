b0ac9a7627c4396fa8572c02331978a5409f7b80
chitrakar1991 <70833683+chitrakar1991@users.noreply.github.com>
2020-11-11 10:49:16 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper {
+    constructor (x, y, radius) {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+
+        this.body = Bodies.circle(x , y, radius, options);
+        this.x=x;
+        this.y=y;
+        this.radius = radius;
+        this.image = loadImage("Images/paperIMG.png");
+        World.add(world, this.body);
+        
+
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(RADIUS);
+        fill("white");
+        
+        imageMode(CENTER);
+        image(this.image, 0, 0, 2*this.radius,2*this.radius);
+        pop();
+    }
+ }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C25-Crumbled-Paper2
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
